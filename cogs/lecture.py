8256723da4b8ff17cb5137c1d02adb89fd76dffe
Lars <larsbeijaard@outlook.com>
2020-11-24 11:06:26 +0100

    Added auth users and weekend days

--- cogs/lecture.py
+++ cogs/lecture.py
@@ -6,33 +6,70 @@ from discord.ext import commands
 
 
 class Lecture(commands.Cog):
+    # The users that can send the bot commands
+    allowed_ids = [368317619838779393]
 
     def __init__(self, client):
         self.client = client
 
+    @commands.command(name='auth')
+    async def auth_user(self, ctx, user_id):
+        if not await self.is_auth_user(ctx):
+            return
+
+        # Add auth user
+        self.allowed_ids.__add__(user_id)
+
+        # Create a new Discord embed
+        embed = discord.Embed(description=f':white_check_mark: User `{user_id}` succesfully added to the auth list!',
+                              color=discord.Color.from_rgb(114, 137, 218))
+
+        message = await ctx.send(embed=embed)
+        await message.delete(delay=3)
+        await ctx.message.delete(delay=3)
+
     @commands.command(name='start')
-    async def lecture(self, ctx):
+    async def lecture(self, ctx, show_now=False, debug_day=-2):
+        if not await self.is_auth_user(ctx):
+            return
+
+        # Create a new Discord embed
         embed = discord.Embed(description='Starting to watch the roster', color=discord.Color.from_rgb(114, 137, 218))
         message = await ctx.send(embed=embed)
         await message.delete(delay=3)
         await ctx.message.delete(delay=3)
 
         while True:
-            while datetime.now().hour != 20:
-                await asyncio.sleep(300)
+            # Allow debugging when 'show_now' is set to 'True'
+            if not show_now:
+                while datetime.now().hour != 20:
+                    await asyncio.sleep(300)
 
             # Get the day index of the week starting from 0
             current_day_in_week_index = datetime.today().weekday()
-            if current_day_in_week_index > 5:
-                current_day_in_week_index = 0
 
+            # See the roster of the day you want starting from 0
+            if debug_day != -2:
+                current_day_in_week_index = debug_day
+
+            # Mention everybody with access to that channel
             await ctx.send('@here')
+
+            # Weekend message (Friday and Saturday)
+            if current_day_in_week_index == 4 or \
+                    current_day_in_week_index == 5:
+                await self.weekend(ctx)
+                return
 
             with open(f'rosters/timetable_2020-11-20.csv', 'r') as csvfile:
                 reader = csv.DictReader(csvfile)
                 for row in reader:
+                    # Show the roster for monday when its Sunday
+                    if current_day_in_week_index == 6:
+                        current_day_in_week_index = -1
+
                     # Continue only if the row is on the correct day in the week
-                    if self.get_lectures_for_day(row["Start day"]) != current_day_in_week_index:
+                    if self.get_lectures_for_day(row["Start day"]) != current_day_in_week_index + 1:
                         continue
 
                     # Check if there are more than 0 teachers, else replace teachers field with 'None'
@@ -60,6 +97,25 @@ class Lecture(commands.Cog):
 
                 # Sleep for 3601 seconds to make sure it only triggers the while loop once
                 await asyncio.sleep(3601)
+
+    async def weekend(self, ctx):
+        embed = discord.Embed(description='It\'s weekend! :partying_face:',
+                              color=discord.Color.from_rgb(114, 137, 218))
+        await ctx.send(embed=embed)
+
+    async def is_auth_user(self, ctx):
+        # If the user is not authenticated, let the user know
+        if ctx.author.id not in self.allowed_ids:
+            # Create a new Discord embed
+            embed = discord.Embed(description=':robot: I can only accept commands from authenticated users!',
+                                  color=discord.Color.from_rgb(114, 137, 218))
+
+            message = await ctx.send(embed=embed)
+            await message.delete(delay=3)
+            await ctx.message.delete(delay=3)
+            return False
+
+        return True
 
     def get_lectures_for_day(self, day: str):
         switch = {
